696530a2becc1b8797548e76d670023d8ea7889c
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-03-18 21:22:35 +0000

    clean up code and refactors

--- src/components/expandingGallery/ExpandingGalleryScrollTo.tsx
+++ src/components/expandingGallery/ExpandingGalleryScrollTo.tsx
@@ -12,8 +12,7 @@ export default function ExpandingGalleryScrollTo({
   children,
 }: ExpandingGalleryScrollToProps) {
   const hash = useHash() || undefined;
-  const { useStore } = useExpandingGalleryStore();
-  const store = useStore();
+  const { store } = useExpandingGalleryStore();
 
   useEffect(() => {
     const previousScrollPosition = store.previousScrollPosition;
@@ -28,42 +27,6 @@ export default function ExpandingGalleryScrollTo({
       block: 'center',
       inline: 'center',
     });
-
-    // const previousScrollPosition = store.previousScrollPosition;
-    // if (previousScrollPosition) {
-    //   window.scrollTo({
-    //     left: previousScrollPosition.x,
-    //     top: previousScrollPosition.y,
-    //     behavior: 'smooth',
-    //   });
-    // }
-
-    // const boundingRect = element.getBoundingClientRect();
-    // // an object with x and y properties that represent the middle of the element
-    // const currentScrollPosition = {
-    //   x: boundingRect.left + boundingRect.width / 2,
-    //   y: boundingRect.top + boundingRect.height / 2,
-    // };
-
-    // window.scrollTo({
-    //   left: currentScrollPosition.x,
-    //   top: currentScrollPosition.y,
-    //   behavior: 'smooth',
-    // });
-
-    // window.scrollTo({ behavior: 'smooth', top: 0, left: 0 });
-
-    // const previousHash = store.previousItemHash;
-    // if (previousHash) {
-    //   console.log('scrolling to previous hash');
-
-    //   const prevElement = document.getElementById(previousHash);
-    //   prevElement?.scrollIntoView({
-    //     behavior: 'smooth',
-    //     block: 'center',
-    //     inline: 'center',
-    //   });
-    // }
   }, [hash]);
 
   return <>{children}</>;
--- src/components/expandingGallery/useExpandingGalleryStore.ts
+++ src/components/expandingGallery/useExpandingGalleryStore.ts
@@ -12,35 +12,10 @@ export default function useExpandingGalleryStore() {
     previousScrollPosition: { x: 0, y: 0 },
   }));
 
-  const setPreviousScrollPosition = useCallback(({ x, y }: Position) => {
+  const setPreviousScrollPosition = ({ x, y }: Position) =>
     useStore.setState({ previousScrollPosition: { x, y } });
-  }, []);
 
-  return { useStore, setPreviousScrollPosition };
+  const store = useStore();
+
+  return { store, setPreviousScrollPosition };
 }
-
-// const useStore = create<ExpandingGalleryState>()(set => ({
-// previousItemHash: undefined,
-// currentItemHash: undefined,
-// previousItemSlug: undefined,
-// currentItemSlug: undefined,
-// previousScrollPosition: { x: 0, y: 0 },
-// currentScrollPosition: { x: 0, y: 0 },
-// setPreviousItemSlug: (slug: string) => set({ previousItemSlug: slug }),
-// setCurrentItemSlug: (slug: string) => set({ currentItemSlug: slug }),
-// setPreviousItemHash: (item: string) => set({ previousItemHash: item }),
-// setCurrentItemHash: (item: string) => set({ currentItemHash: item }),
-// setPreviousScrollPosition: (position: Position) =>
-// set({ previousScrollPosition: position }),
-// setCurrentScrollPosition: (position: Position) =>
-//   set({ currentScrollPosition: position }),
-// }));
-
-// export const setPreviousScrollPosition = ({ x, y }: Position) =>
-//   useStore.setState({ previousScrollPosition: { x, y } });
-
-// export const setPreviousScrollPosition = useCallback(({ x, y }: Position) => {
-//   useStore.setState({ previousScrollPosition: { x, y } });
-// }, []);
-
-// export default { useStore, setPreviousScrollPosition };
