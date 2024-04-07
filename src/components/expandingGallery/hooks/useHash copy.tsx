72a46b4f4c3687ad122c6987c5b7cfcd3a3aa5a2
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-04-07 13:50:20 +0100

    clean up and improve implementation of the useHash hook

---
+++ src/components/expandingGallery/hooks/useHash copy.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useEffect, useRef, useState, useMemo } from 'react';
+
+type Hash = string | null;
+
+// Hook to persist state variables in the URL hash
+export default function useHash(): [Hash, (newState: Hash) => void] {
+  const [hash, updateHash] = useState<Hash>(null);
+  const setHashRef = useRef<((newState: string | null) => void) | null>(null);
+
+  useEffect(() => {
+    // console.log('running useHash');
+
+    setHashRef.current = function (newState: Hash) {
+      if (newState === null) {
+        const url = window.location.toString();
+        const cleanUrl = url.substring(0, url.indexOf('#'));
+        window.history.replaceState({}, document.title, cleanUrl);
+      }
+      if (newState) window.location.hash = newState;
+
+      updateHash(newState);
+    };
+  }, [hash]);
+
+  function setHash(newState: Hash): void {
+    if (setHashRef.current) {
+      setHashRef.current(newState);
+      return;
+    }
+    updateHash(newState);
+  }
+
+  return [hash, setHash];
+}
--- src/components/expandingGallery/hooks/useHash.tsx
+++ src/components/expandingGallery/hooks/useHash.tsx
@@ -1,36 +1,42 @@
 'use client';
 
-import { useEffect, useRef, useState, useMemo } from 'react';
+import { type Dispatch, type SetStateAction, useEffect, useState } from 'react';
 
 type Hash = string | null;
 
-// Hook to persist state variables in the URL hash
-export default function useHash(): [Hash, (newState: Hash) => void] {
-  const [hash, updateHash] = useState<Hash>(null);
-  const setHashRef = useRef<((newState: string | null) => void) | null>(null);
+/**
+ * Get the current hash from the URL, excluding the '#' character.
+ * @returns A string with the current hash or null if there is no hash.
+ */
+function getUrlHash(): Hash {
+  return window.location.hash.slice(1) || null;
+}
+
+/**
+ * Sets the URL hash parameter without causing a page reload.
+ * If the hash is null, the URL hash is removed.
+ * @param hash A string representing the new hash or null to remove the hash.
+ */
+function setUrlHash(hash: Hash): void {
+  if (hash === null) {
+    const url = window.location.toString();
+    const cleanUrl = url.substring(0, url.indexOf('#'));
+    window.history.replaceState({}, document.title, cleanUrl);
+  } else {
+    window.location.hash = hash;
+  }
+}
+
+/**
+ * Hook to persist state variables in the URL hash
+ * @returns A tuple with the current hash and a function to update it.
+ */
+export default function useHash(): [Hash, Dispatch<SetStateAction<Hash>>] {
+  const [hash, setHash] = useState<Hash>(() => getUrlHash());
 
   useEffect(() => {
-    // console.log('running useHash');
-
-    setHashRef.current = function (newState: Hash) {
-      if (newState === null) {
-        const url = window.location.toString();
-        const cleanUrl = url.substring(0, url.indexOf('#'));
-        window.history.replaceState({}, document.title, cleanUrl);
-      }
-      if (newState) window.location.hash = newState;
-
-      updateHash(newState);
-    };
+    setUrlHash(hash);
   }, [hash]);
-
-  function setHash(newState: Hash): void {
-    if (setHashRef.current) {
-      setHashRef.current(newState);
-      return;
-    }
-    updateHash(newState);
-  }
 
   return [hash, setHash];
 }
