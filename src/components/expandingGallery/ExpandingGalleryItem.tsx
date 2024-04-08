55733709277f70167417c027decf184ea6d550c9
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-04-08 10:22:24 +0100

    Add hooks to expanding gallery item to make handling click events more flexible

--- src/components/expandingGallery/ExpandingGalleryItem.tsx
+++ src/components/expandingGallery/ExpandingGalleryItem.tsx
@@ -10,7 +10,10 @@ import { useExpandingGallery } from './contexts/ExpandingGalleryContext';
 
 export type ExpandingGalleryItemProps<T extends ElementType> = {
   uniqueSlug: string;
-  onClick?: (e: React.MouseEvent<Element, MouseEvent>) => void;
+  onClick?: (e?: React.MouseEvent<Element, MouseEvent>) => void;
+  afterHandleClick?: (e?: React.MouseEvent<Element, MouseEvent>) => void;
+  beforeHandleClick?: (e?: React.MouseEvent<Element, MouseEvent>) => void;
+  onHandleClick?: (e?: React.MouseEvent<Element, MouseEvent>) => void;
   className?: string;
   as?: T;
   children: ReactNode;
@@ -19,6 +22,15 @@ export type ExpandingGalleryItemProps<T extends ElementType> = {
 // A generic component that can become any HTML element or another ReactComponent
 export default function ExpandingGalleryItem<U extends ElementType>({
   uniqueSlug,
+  afterHandleClick = () => {
+    /**/
+  },
+  beforeHandleClick = () => {
+    /**/
+  },
+  onHandleClick = () => {
+    /**/
+  },
   onClick,
   as,
   children,
@@ -30,19 +42,31 @@ export default function ExpandingGalleryItem<U extends ElementType>({
     useExpandingGallery();
 
   function handleClick(e: React.MouseEvent<Element, MouseEvent>) {
+    // hook to allow for custom behavior at the beginning of the handleClick function
+    beforeHandleClick(e);
+
+    // prevent the default behavior of the click event
     e.preventDefault();
     e.stopPropagation();
 
+    // hook to allow for custom behavior at the end of the handleClick function
+    onHandleClick(e);
+
+    // store the current scroll position
     setPreviousScrollPosition({
       scrollX: window.scrollX,
       scrollY: window.scrollY,
     });
 
+    // if the current unique slug is the same as the one clicked, set it to null
     if (currentUniqueSlug === uniqueSlug) {
       setCurrentUniqueSlug(null);
       return;
     }
     setCurrentUniqueSlug(uniqueSlug);
+
+    // hook to allow for custom behavior after the handleClick function
+    afterHandleClick(e);
   }
 
   return (
