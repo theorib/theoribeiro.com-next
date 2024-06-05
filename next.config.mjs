50ee6cb5a978303c6559905e9e81da0461938057
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-06-05 20:32:46 +0100

    feat(next.config.mjs): add strict content security policy (csp)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 import withExportImages from 'next-export-optimize-images';
 // import withBundleAnalyzer from '@next/bundle-analyzer';
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -24,7 +37,20 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // Set strict content security policies
+  headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withExportImages(nextConfig);
-// export default withBundleAnalyzer(withExportImages(nextConfig));
