2f696f83cebd54a6a873a67991481891f233b9c2
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-06-05 14:37:08 +0100

    fix(tsconfig.json): update files to be scanned

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,24 @@
-const config={extends:["@commitlint/config-conventional"],parserPreset:"conventional-changelog-conventionalcommits",rules:{"type-enum":[2,"always",["feat","fix","docs","style","chore","refactor","ci","test","revert","perf","vercel"]],}};export default config;
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'chore',
+        'refactor',
+        'ci',
+        'test',
+        'revert',
+        'perf',
+        'vercel',
+      ],
+    ],
+  },
+};
+export default config;
