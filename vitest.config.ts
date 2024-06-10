c5494f74015b3ec6480671a5eaa87f5c07dcf780
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-06-10 14:12:28 +0100

    fix(vitest.config.ts): fix typescript error with vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vitest/config';
+import { UserConfig, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()] as UserConfig['plugins'],
   test: {
     globals: true,
     environment: 'jsdom',
