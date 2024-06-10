be919feec46daf91fe454acce75ad2daf5d53825
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-06-10 21:08:38 +0100

    refactor(env.ts): remove commented code

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-// import { fromError } from 'zod-validation-error';
+
 import { createEnv } from '@t3-oss/env-nextjs';
 
 export const env = createEnv({
@@ -22,27 +22,7 @@ export const env = createEnv({
     NEXT_PUBLIC_REMOTE_ASSETS_PATH: process.env.NEXT_PUBLIC_REMOTE_ASSETS_PATH,
   },
 
-  // clientPrefix: 'NEXT_PUBLIC_',
-  // runtimeEnv: process.env,
   emptyStringAsUndefined: true,
 });
-//   const EnvSchema = z.object({
-//     NEXT_PUBLIC_ASSETS_PATH: z.union([z.string().url(), z.literal('/')]),
-//     NEXT_PUBLIC_LOCAL_ASSETS_PATH: z.union([z.string().url(), z.literal('/')]),
-//     NEXT_PUBLIC_REMOTE_ASSETS_PATH: z.string().url(),
-//   });
-//   let envPublic: z.infer<typeof EnvSchema>;
-//   try {
-//     envPublic = EnvSchema.parse({
-//       NEXT_PUBLIC_ASSETS_PATH: process.env.NEXT_PUBLIC_ASSETS_PATH,
-//       NEXT_PUBLIC_LOCAL_ASSETS_PATH: process.env.NEXT_PUBLIC_LOCAL_ASSETS_PATH,
-//       NEXT_PUBLIC_REMOTE_ASSETS_PATH:
-//         process.env.NEXT_PUBLIC_REMOTE_ASSETS_PATH,
-//     });
-//   } catch (err) {
-//     const validationError = fromError(err);
-//     throw validationError;
-//   }
-// }
 
 export default env;
