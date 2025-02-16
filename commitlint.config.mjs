3a026735fec44b1e3680739dcd2ec0ac6ec7a825
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-16 14:05:27 +0000

    chore(husky): update commit-msg hook to use pnpm   - switch from npx to pnpm for running commitlint in commit-msg hook
    ♻️ refactor(commitlint): change parser preset to atom
      - update commitlint config to use conventional-changelog-atom preset
    
    build(deps): add conventional-changelog-atom
      - include conventional-changelog-atom as a dev dependency

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,6 @@
 const config = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-conventionalcommits',
+  parserPreset: 'conventional-changelog-atom',
   rules: {
     'type-enum': [
       2,
