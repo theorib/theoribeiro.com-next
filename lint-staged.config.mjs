bc201c1276ed72d7aa770c8bd9e969b1041246d0
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-17 21:40:31 +0000

    chore(lint-staged): suppress eslint warnings during pre-commit
    - add `--quiet` flag to eslint command in lint-staged config
    - warnings will still be visible when running eslint directly

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 const config = {
-  '**/*.{js,jsx,ts,tsx}': ['eslint --fix'],
+  '**/*.{js,jsx,ts,tsx}': ['eslint --fix --quiet'],
   '**/*.{ts,tsx}': () => 'tsc --noEmit --skipLibCheck',
   '**/*.{html,json,css,scss,md,mdx,js,jsx,ts,tsx}': ['prettier -w'],
 };
